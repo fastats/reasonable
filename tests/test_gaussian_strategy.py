8c223236d52d629f1ee93862d8491f477082a76d
Dave Willmer <dave.willmer@gmail.com>
2017-09-09 14:02:07 +0100

    Add Kolmogorov-Smirnov tests

--- tests/test_gaussian_strategy.py
+++ tests/test_gaussian_strategy.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+from scipy.stats import kstest
+
 from reasonable.strategies import GaussianStrategy
 
 
@@ -15,6 +18,19 @@ def test_truncation():
     strat = GaussianStrategy(0, 1, 0.5, None, 8)
     assert 0.5 <= strat.do_draw(None) < 10
 
+
+def test_kolmogorov_smirnov_fit():
+    """
+    Kolmogorov-Smirnov tests the goodness of fit
+    of the data to a given distribution.
+    """
+    strat = GaussianStrategy(0.0, 1.0, None, None, 8)
+    data = [strat.do_draw(None) for x in range(10000)]
+    arr = np.array(data, dtype='float32')
+
+    t_stat, p_value = kstest(arr, 'norm')
+
+    assert p_value < 0.95
 
 if __name__ == '__main__':
     import pytest
