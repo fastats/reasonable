92effc9e48973b2608e4c2304cf6232304bf3b4d
Dave Willmer <dave.willmer@gmail.com>
2017-09-10 23:36:41 +0100

    Add setup.py and packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup
+
+
+setup(
+  name='reasonable',
+  packages=['reasonable'],
+  version='2017.1',
+  description='Reasonable float handling for Hypothesis testing',
+  author='Dave Willmer',
+  author_email='dave.willmer@gmail.com',
+  url='https://github.com/fastats/reasonable',
+  download_url='https://github.com/fastats/reasonable/archive/2017.1.tar.gz',
+  keywords=['reasonable', 'hypothesis', 'generative testing'],
+  classifiers=[]
+)
