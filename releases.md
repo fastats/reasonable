e304d74c0eccde2fd41c94c09161f86ad6920ef8
Dave Willmer <dave.willmer@gmail.com>
2017-09-11 00:11:53 +0100

    Add setup.py and packaging (#6)
    * Add setup.py and packaging
    
    * Fix setup.py
    
    * Change pytest defaults

---
+++ releases.md
@@ -0,0 +1,16 @@
+
+2017.1
+------
+
+### New features/Deprecations
+
+- `floats` function for generating float values - ``gh#1``
+- Comprehensive unit-test coverage - ``gh#2``
+- Moved from coveralls to code cov - ``gh#3``
+- `gaussian` function for gaussian-distributed floats - ``gh#4``
+- Added docs using sphinx - ``gh#5``
+
+### Bug Fixes
+
+
+### Enhancements
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+
+
+MAJOR = 2017
+MINOR = 1
+MICRO = ''
+
+version = '.'.join(map(str, [MAJOR, MINOR]))
+if MICRO:
+  version += MICRO
+
+
+def setup_package():
+    setup(
+      name='reasonable',
+      packages=['reasonable'],
+      version=version,
+      description='Reasonable float handling for Hypothesis testing',
+      author='Dave Willmer',
+      author_email='dave.willmer@gmail.com',
+      url='https://github.com/fastats/reasonable',
+      download_url='https://github.com/fastats/reasonable/archive/2017.1.tar.gz',
+      keywords=['reasonable', 'hypothesis', 'generative testing'],
+      classifiers=[]
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
